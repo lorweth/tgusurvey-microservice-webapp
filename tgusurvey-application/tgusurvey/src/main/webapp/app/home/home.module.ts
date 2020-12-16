7440a5e9598b3f91f3e95cdc23227eda5b9b2b65
witcher-creator <dhuuloc8818@gmail.com>
2020-12-16 17:56:16 +0700

    #2: add ngx-charts

--- tgusurvey-application/tgusurvey/src/main/webapp/app/home/home.module.ts
+++ tgusurvey-application/tgusurvey/src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 import { TgusurveySharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
-  imports: [TgusurveySharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [TgusurveySharedModule, RouterModule.forChild([HOME_ROUTE]), BrowserAnimationsModule, NgxChartsModule],
   declarations: [HomeComponent],
 })
 export class TgusurveyHomeModule {}
